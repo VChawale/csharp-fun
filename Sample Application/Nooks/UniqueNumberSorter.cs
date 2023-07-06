07fe0487373dcb51af16ea967cb7211c0d05be33
Vaibhao Chawale <vchawale@adagetech.com>
2023-07-06 18:57:20 -0500

    Worked on sorting unique 5 numbers nook

---
+++ Sample Application/Nooks/UniqueNumberSorter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Application.Nooks
+{
+    internal static class UniqueNumberSorter
+    {
+        internal static void SortUniqueNumbers()
+        {
+            List<int> numberList = new List<int>();
+
+            while (true)
+            {
+
+                if (numberList.Count < 5)
+                {
+                    Console.WriteLine("Enter new number!");
+                    var inputValue = Console.ReadLine();
+                    if (int.TryParse(inputValue, out int number))
+                    {
+                        //if number found in list
+                        if (numberList.IndexOf(number) == -1)
+                        {
+                            numberList.Add(number);
+                            continue;
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input, Enter Valid Number!");
+                        continue;
+                    }
+                }
+                else
+                {
+
+                    numberList.Sort();
+                    Console.WriteLine("Below is sorted list of numbers!");
+                    foreach (var number in numberList)
+                    {
+                        Console.WriteLine(number);
+                    }
+                    break;
+
+                }
+            }
+        }
+    }
+}
--- Sample Application/Program.cs
+++ Sample Application/Program.cs
@@ -2,5 +2,7 @@
 //Sample_Application.Nooks.FbLike.Like();
 
 //Test Reverse My Name Feature
+//Sample_Application.Nooks.ReverseMyName.reverseMyName();
 
-Sample_Application.Nooks.ReverseMyName.reverseMyName();
+//Test Sorted Number list
+Sample_Application.Nooks.UniqueNumberSorter.SortUniqueNumbers();
